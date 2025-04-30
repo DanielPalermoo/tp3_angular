ba284587ce1055773acbfb332a2c43b4eb4bc6d4
DanielPalermoo <45328550@fi.unju.edu.ar>
2025-04-29 23:14:16 -0300

    Proyecto creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Punto1Component } from './components/public/punto1/punto1.component';
+import { Punto2Component } from './components/public/punto2/punto2.component';
+import { Punto3Component } from './components/public/punto3/punto3.component';
+import { HomeComponent } from './components/home/home.component';
+export const routes: Routes = [
+    {path:'home', component: HomeComponent},
+    {path:'punto1', component: Punto1Component},
+    {path:'punto2', component: Punto2Component},
+    {path:'punto3', component: Punto3Component},
+    {path:'**', pathMatch: 'full', redirectTo: 'home'}
+];
