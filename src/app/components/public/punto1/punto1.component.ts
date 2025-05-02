fe79c66b06ada9e182902af0b5c0ddd64e6af887
DanielPalermoo <45328550@fi.unju.edu.ar>
2025-05-02 00:38:52 -0300

    Punto 2 realizado2

--- src/app/components/public/punto1/punto1.component.ts
+++ src/app/components/public/punto1/punto1.component.ts
@@ -14,19 +14,19 @@ export class Punto1Component {
       titulo: 'Alex "Poatan" Pereira',
       descripcion:
         'Peleador brasileño de peso semipesado, conocido por su poderoso striking y su pasado como campeón de kickboxing. Ha sido campeón en dos divisiones de UFC',
-      imagen: 'assets/img/alex_pereira.jpg',
+      imagen: 'assets/img/punto1/alex_pereira.jpg',
     },
     {
       titulo: 'Israel Adesanya',
       descripcion:
         'Peleador de peso medio con un estilo técnico y llamativo, combina artes marciales mixtas con movimientos de kickboxing.',
-      imagen: 'assets/img/israel_adesanya.jpg',
+      imagen: 'assets/img/punto1/israel_adesanya.jpg',
     },
     {
       titulo: 'Alexander Volkanovski',
       descripcion:
         'Campeón de peso pluma, célebre por su resistencia y su habilidad táctica en cada pelea.',
-      imagen: 'assets/img/alexander_volkanovski.jpg',
+      imagen: 'assets/img/punto1/alexander_volkanovski.jpg',
     },
   ];
 
--- src/app/components/public/punto2/punto2.component.ts
+++ src/app/components/public/punto2/punto2.component.ts
@@ -1,11 +1,89 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-punto2',
-  imports: [],
+  imports: [CommonModule],
+  standalone:true,
   templateUrl: './punto2.component.html',
   styleUrl: './punto2.component.css'
 })
 export class Punto2Component {
 
+  productos = [
+    {
+      nombre: 'Guantes de boxeo Proyec' , 
+      descripcion: 'Los guantes Proyec ofrecen una combinación perfecta de durabilidad y comodidad, ideales para trabajos exigentes. Su diseño ergonómico y materiales de alta calidad garantizan un ajuste seguro y una excelente protección, sin sacrificar la destreza.', 
+      img: 'assets/img/punto2/guantes_proyec.jpg', 
+      precio: 90000
+    },
+    {
+      nombre: 'Guantes de boxeo Proyec' , 
+      descripcion: 'Los guantes Proyec ofrecen una combinación perfecta de durabilidad y comodidad, ideales para trabajos exigentes. Su diseño ergonómico y materiales de alta calidad garantizan un ajuste seguro y una excelente protección, sin sacrificar la destreza.', 
+      img: 'assets/img/punto2/guantes_proyec.jpg', 
+      precio: 90000
+    },
+    {
+      nombre: 'Guantes de boxeo Proyec' , 
+      descripcion: 'Los guantes Proyec ofrecen una combinación perfecta de durabilidad y comodidad, ideales para trabajos exigentes. Su diseño ergonómico y materiales de alta calidad garantizan un ajuste seguro y una excelente protección, sin sacrificar la destreza.', 
+      img: 'assets/img/punto2/guantes_proyec.jpg', 
+      precio: 90000
+    },
+    {
+      nombre: 'Guantes 2M' , 
+      descripcion: 'Los guantes 2M están diseñados para brindar máxima protección y confort en diversas aplicaciones. Con su tecnología avanzada y materiales resistentes, son perfectos para el uso diario, asegurando un agarre firme y una larga vida útil.', 
+      img: 'assets/img/punto2/guantes_2m.jpg', 
+      precio: 50000
+    },
+    {
+      nombre: 'Guantes 2M' , 
+      descripcion: 'Los guantes 2M están diseñados para brindar máxima protección y confort en diversas aplicaciones. Con su tecnología avanzada y materiales resistentes, son perfectos para el uso diario, asegurando un agarre firme y una larga vida útil.', 
+      img: 'assets/img/punto2/guantes_2m.jpg', 
+      precio: 50000
+    },
+    {
+      nombre: 'Guantes 2M' , 
+      descripcion: 'Los guantes 2M están diseñados para brindar máxima protección y confort en diversas aplicaciones. Con su tecnología avanzada y materiales resistentes, son perfectos para el uso diario, asegurando un agarre firme y una larga vida útil.', 
+      img: 'assets/img/punto2/guantes_2m.jpg', 
+      precio: 50000
+    },
+    {
+      nombre: 'Guantes Adidas' , 
+      descripcion: 'Los guantes Adidas destacan por su estilo moderno y funcionalidad. Confeccionados con materiales premium, ofrecen un ajuste perfecto y un excelente rendimiento, ideales para deportistas que buscan tanto protección como comodidad en sus actividades.', 
+      img: 'assets/img/punto2/guantes_adidas.jpg', 
+      precio: 100000
+    },
+    {
+      nombre: 'Guantes Adidas' , 
+      descripcion: 'Los guantes Adidas destacan por su estilo moderno y funcionalidad. Confeccionados con materiales premium, ofrecen un ajuste perfecto y un excelente rendimiento, ideales para deportistas que buscan tanto protección como comodidad en sus actividades.', 
+      img: 'assets/img/punto2/guantes_adidas.jpg', 
+      precio: 100000
+    },
+    {
+      nombre: 'Guantes Adidas' , 
+      descripcion: 'Los guantes Adidas destacan por su estilo moderno y funcionalidad. Confeccionados con materiales premium, ofrecen un ajuste perfecto y un excelente rendimiento, ideales para deportistas que buscan tanto protección como comodidad en sus actividades.', 
+      img: 'assets/img/punto2/guantes_adidas.jpg', 
+      precio: 100000
+    },
+  ];
+
+carrito: any[] = []; 
+
+constructor() {}
+
+ngOnInit(): void{}
+
+agregarAlCarrito(producto: any): void{
+  this.carrito.push(producto);
+  console.log('Carrito:', this.carrito);
+}
+
+
+calcularTotal(): number {
+  let total = 0;
+  for( let item of this.carrito){
+    total += item.precio;
+  }
+  return total;
+}
 }
